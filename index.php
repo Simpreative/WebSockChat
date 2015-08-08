1aaa3f04c5b2c827868507e2b7bbefc26135fe04
ZerglingGo <zerglinggo@zerglinggo.net>
2015-08-09 03:56:06 +0900

    sort source

--- index.php
+++ index.php
@@ -1,21 +1,22 @@
 <!DOCTYPE html>
-<title>WebSocket Test Page</title>
+<html>
+<head>
+<title>WebSocket Chatting</title>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.js"></script>
 <script type="text/javascript" src="http://pe1.me/data/public/common/function.js"></script>
 <script type="text/javascript">
 function microtime(get_as_float) {
-  //  discuss at: http://phpjs.org/functions/microtime/
-  // original by: Paulo Freitas
-  //   example 1: timeStamp = microtime(true);
-  //   example 1: timeStamp > 1000000000 && timeStamp < 2000000000
-  //   returns 1: true
+	//  discuss at: http://phpjs.org/functions/microtime/
+	// original by: Paulo Freitas
+	//   example 1: timeStamp = microtime(true);
+	//   example 1: timeStamp > 1000000000 && timeStamp < 2000000000
+	//   returns 1: true
 
-  var now = new Date()
-    .getTime() / 1000;
-  var s = parseInt(now, 10);
+	var now = new Date().getTime() / 1000;
+	var s = parseInt(now, 10);
 
-  return (get_as_float) ? now : (Math.round((now - s) * 1000) / 1000) + ' ' + s;
+	return (get_as_float) ? now : (Math.round((now - s) * 1000) / 1000) + ' ' + s;
 }
 </script>
 <script type="text/javascript">
@@ -25,7 +26,6 @@ function microtime(get_as_float) {
 	wSocket.onopen = function(e){ addOutput("서버 연결 완료"); }
 	wSocket.onclose = function(e){ addOutput("서버 연결 종료"); }
 	function send(x){ 
-
 		if(x == "!핑"){
 			wSocket.send("핑 요청 - " + microtime(true) + "\n");
 			return;
@@ -45,7 +45,6 @@ function microtime(get_as_float) {
 
 	$(document).ready(function(){ 
 		$("#inputMessage").bind("keypress",function(event){ 
-
 			if(event.keyCode == 13){
 				if($("#inputMessage").val() == "") {
 					return false;
@@ -57,7 +56,9 @@ function microtime(get_as_float) {
 		});
 	});
 </script>
-
-<input type="text" id="inputMessage">
-<button id="sendButton">Send</button>
-<div id="output" style="height: 800px; overflow: scroll; overflow-x: hidden;"></div>
+</head>
+<body>
+	<div id="output" style="width: 100%; height: 90%; overflow: scroll; overflow-x: hidden;"></div>
+	<input type="text" id="inputMessage" style="width: 100%;">
+</body>
+</html>
