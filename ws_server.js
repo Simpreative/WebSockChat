4f28522bf6d9a1732a35c00b7d5c53414975eb8e
ZerglingGo <zerglinggo@zerglinggo.net>
2015-08-09 00:22:27 +0900

    broadcast test

--- ws_server.js
+++ ws_server.js
@@ -1,12 +1,18 @@
 var ws = require("nodejs-websocket")
  
+function broadcast(server, msg) {
+    server.connections.forEach(function (conn) {
+        conn.sendText(msg)
+    })
+}
+
 var server = ws.createServer(function (conn) {
     console.log("New connection")
     conn.on("text", function (str) {
-        console.log("Received "+str)
-        conn.sendText(str)
+        console.log("Received " + str)
+        broadcast(self, str)
     })
     conn.on("close", function (code, reason) {
         console.log("Connection closed")
     })
-}).listen(8000)
+}).listen(80)
