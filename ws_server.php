06048ebd44901db50d1c92f9a2997991f2c57b9a
ZerglingGo <zerglinggo@zerglinggo.net>
2015-08-08 23:33:25 +0900

    add annotation

--- ws_server.php
+++ ws_server.php
@@ -1,8 +1,16 @@
 <?php
+/**
+ * WebSocket Server
+ * 
+ * @author ZerglingGo <zerglinggo@zerglinggo.net>
+ * @license http://opensource.org/licenses/MIT MIT License
+ */
+
 if(php_sapi_name() !== 'cli') {
 	header("HTTP/1.1 403 Forbidden");
 	echo "403 Forbidden";
 	exit;
 }
 
+echo "WebSocket Server";
 ?>
