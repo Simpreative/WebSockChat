2a03b989d1a087bae9c972acf8491a3bd2639227
ZerglingGo <zerglinggo@zerglinggo.net>
2015-08-09 01:17:25 +0900

    Sorting source code

--- ws_server.js
+++ ws_server.js
@@ -1,21 +1,23 @@
 var ws = require("nodejs-websocket")
 
 var server = ws.createServer(function (connection) {
-        connection.nickname = null
-        connection.on("text", function (str) {
-                if (connection.nickname === null) {
-                        connection.nickname = str
-                        broadcast(str+" entered")
-                } else
-                        broadcast("["+connection.nickname+"] "+str)
-        })
-        connection.on("close", function () {
-                broadcast(connection.nickname+" left")
-        })
+	connection.nickname = null
+	connection.on("text", function (str) {
+			if (connection.nickname === null) {
+				connection.nickname = str
+				broadcast(str + " entered")
+			} else {
+				broadcast("[" + connection.nickname + "] " + str)
+			}
+		}
+	})
+	connection.on("close", function () {
+		broadcast(connection.nickname + " left")
+	})
 }).listen(8000)
 
 function broadcast(str) {
-        server.connections.forEach(function (connection) {
-                connection.sendText(str)
-        })
+	server.connections.forEach(function (connection) {
+		connection.sendText(str)
+	})
 }
