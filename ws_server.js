c521ac7ccb91ebc103d0ab470f0bde4415ae7b53
ZerglingGo <zerglinggo@zerglinggo.net>
2015-08-09 00:28:54 +0900

    broadcast test

--- ws_server.js
+++ ws_server.js
@@ -10,7 +10,7 @@ var server = ws.createServer(function (conn) {
     console.log("New connection")
     conn.on("text", function (str) {
         console.log("Received " + str)
-        broadcast(self, str)
+        broadcast(this, str)
     })
     conn.on("close", function (code, reason) {
         console.log("Connection closed")
