5cd707a519dda83dcc1d90152c399a90e1028c31
ZerglingGo <zerglinggo@zerglinggo.net>
2015-08-09 01:14:42 +0900

    Revert source

--- ws_server.js
+++ ws_server.js
@@ -1,28 +1,21 @@
 var ws = require("nodejs-websocket")
 
-var server = ws.createServer(onConnect).listen(8000)
-
-function onConnect(connection) {
-	connection.nickname = null
-	connection.on("text", onText)
-	connection.on("close", onClose)
-}
-
-function onText(connection, str) {
-	if (connection.nickname === null) {
-		connection.nickname = str
-		broadcast(str+" entered")
-	} else {
-		broadcast("["+connection.nickname+"] "+str)
-	}
-}
-
-function onClose(connection) {
-	broadcast(connection.nickname + " left")
-}
+var server = ws.createServer(function (connection) {
+        connection.nickname = null
+        connection.on("text", function (str) {
+                if (connection.nickname === null) {
+                        connection.nickname = str
+                        broadcast(str+" entered")
+                } else
+                        broadcast("["+connection.nickname+"] "+str)
+        })
+        connection.on("close", function () {
+                broadcast(connection.nickname+" left")
+        })
+}).listen(8000)
 
 function broadcast(str) {
-	server.connections.forEach(function (connection) {
-		connection.sendText(str)
-	})
+        server.connections.forEach(function (connection) {
+                connection.sendText(str)
+        })
 }
