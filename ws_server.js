ac18ee7d21944fa66b7e9edefcc1b583054ad81d
Ketpaku <ketpaku@ketpaku.cf>
2015-08-09 04:14:46 +0900

    timeout

--- ws_server.js
+++ ws_server.js
@@ -3,21 +3,23 @@ var microtime = require("microtime");
 var util = require("util");
 var server = ws.createServer(function (connection) {
 	connection.nickname = null;
+	connection.timerout = setTimeout(connection.timeout,5000);
+	connection.timeout = function(){ connection.close(1011,"PING TIMEOUT"); }
 	connection.on("text", function (str) {
+
+			if(str == "PING"){
+				connection.timerout = setTimeout(connection.timeout,5000);
+				return;
+			}
+
 			if (connection.nickname === null) {
 				connection.nickname = str;
 				broadcast(str + " entered");
 			} else {
 
-				var or = /^핑\s요청\s\-\s(.*)/;
-				var os = str.match(or);
-				if(os){
-						console.log(util.inspect(os));
-						console.log(microtime.nowDouble());
-						broadcast("[" + connection.nickname + "] 응답 : 0." + (microtime.nowDouble() - os[1]) + "초");
-				} else {
+
 					broadcast("[" + connection.nickname + "] " + str);
-				}
+				
 			}
 		}
 	)
