b72706e77ae1f905c284c934d2f500a147076748
ZerglingGo <zerglinggo@zerglinggo.net>
2015-08-10 15:11:53 +0900

    Added server log time

--- ws_server.js
+++ ws_server.js
@@ -2,6 +2,7 @@ var ws = require("nodejs-websocket");
 var Autolinker = require('autolinker');
 var microtime = require("microtime");
 var util = require("util");
+var fs = require("fs");
 
 function htmlspecialchars(string,quote_style,charset,double_encode){var optTemp=0,i=0,noquotes=false;if(typeof quote_style==='undefined'||quote_style===null){quote_style=2}string=string.toString();if(double_encode!==false){string=string.replace(/&/g,'&amp;')}string=string.replace(/</g,'&lt;').replace(/>/g,'&gt;');var OPTS={'ENT_NOQUOTES':0,'ENT_HTML_QUOTE_SINGLE':1,'ENT_HTML_QUOTE_DOUBLE':2,'ENT_COMPAT':2,'ENT_QUOTES':3,'ENT_IGNORE':4};if(quote_style===0){noquotes=true}if(typeof quote_style!=='number'){quote_style=[].concat(quote_style);for(i=0;i<quote_style.length;i++){if(OPTS[quote_style[i]]===0){noquotes=true}else if(OPTS[quote_style[i]]){optTemp=optTemp|OPTS[quote_style[i]]}}quote_style=optTemp}if(quote_style&OPTS.ENT_HTML_QUOTE_SINGLE){string=string.replace(/'/g,'&#039;')}if(!noquotes){string=string.replace(/"/g,'&quot;')}return string}
 
@@ -108,3 +109,10 @@ function broadcast(str) {
 		connection.sendText(str);
 	})
 }
+
+function getTime() {
+	var date = new Date();
+	dateFormat("yyyy-mm-dd HH:MM:ss");
+}
+
+eval(fs.readFileSync('date.format.js')+'');
